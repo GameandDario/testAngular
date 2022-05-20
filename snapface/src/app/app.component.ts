04065e562266d96d8df15dc3cb458a59bd460c08
Stéphane <stephane.sokeo@gmail.com>
2022-05-20 17:52:56 +0200

    add general style & dynamic style w/ ngStyle

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -28,7 +28,7 @@ export class AppComponent implements OnInit {
       imageUrl:
         'https://images.unsplash.com/photo-1439405326854-014607f694d7?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170',
       season: 'Printemps',
-      snaps: 5,
+      snaps: 125,
       location: 'Mexique',
     },
     {
